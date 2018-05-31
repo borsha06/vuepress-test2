7ce11797dcc88502cc99d74d0a81e4b0cd7c585e
borsha06 <tasfia.sharmin1994@gmail.com>
2018-06-01 03:18:23 +0600

    added modal

---
+++ .vuepress/components/modal.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="modal">
+        <div class="modal" @click="modal">
+            <strong>Click Here!!!</strong>
+        </div>
+
+    </div>
+
+
+</template>
+<script>
+    import Swal from 'sweetalert2'
+    export default{
+
+        data(){
+            return {
+
+                message:"Hello Vuepress"
+            }
+        },
+        methods: {
+
+
+            modal: function () {
+                Swal(this.message)            }
+        }
+    }
+
+</script>
+<style>
+    .modal{
+        color:#50b07c;
+        font-size: 30px;
+    }
+</style>
